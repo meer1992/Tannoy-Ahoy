97993ae880ae5db6c9c3262a15a4a438ef65c661
shash678 <shash678@outlook.com>
2015-04-27 22:44:13 +1200

    Tannoy Ahoy! Android App that is made on Android Studio

---
+++ TannoyAhoy!/app/src/androidTest/java/com/uow/tannoyahoy/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.uow.tannoyahoy;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
