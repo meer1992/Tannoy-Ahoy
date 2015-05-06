1ae82d15fc9ae108ec1dc3d6bed83491528adc85
mitchgrout <mitchell.grout@hotmail.com>
2015-05-07 00:18:59 +1200

    Improvements to app.d, bad requests throw exceptions with HTTP error codes

--- tannoyserver/source/app.d
+++ tannoyserver/source/app.d
@@ -3,125 +3,164 @@ import vibe.core.log;
 import vibe.http.router;
 import vibe.http.server;
 import vibe.web.rest;
+import vibe.data.json;
 import std.datetime, core.time;
- 
-//Easy ways to populate the server with data using curl:
-// $ curl -H "Content-Type: application/json" -X GET localhost:8080/queue
-// $ curl -H "Content-Type: application/json" -X PUT -d '{"message":"this is a test message"}' localhost:8080/add
 
-//Using Java:
-// GET: http://stackoverflow.com/a/1485730
-// PUT: http://stackoverflow.com/a/1051105
-
-//For getting the next unique ID of a message
-static int next_id = 0;
+//TODO:
+// - User/Password 
+// 	- throw 401 or 418 if unauthorised to perform an action
 
 struct ResponseQueue {
-	this(string server_name){ this.server_name = server_name; }
-	string server_name;
-	Response[] queue;
+	this(string name){ this.name = name; }
+	
+	//Track the server name	
+	string name;
+
+	//Hashmap of responses (ID maps to message)
+	Response[int] queue;
+
+	//Each server has an individual ID counter
+	int next_id = 0;
 }
 
 struct Response{
-	this(SysTime time, string message, string sender){ 
+	this(SysTime time, int ID, string message, string sender){ 
 		this.time = time; 
-		this.message = message; 
-		this.ID = next_id++; 
+		this.ID = ID;
+		this.message = message;  
 		this.sender = sender;
 	}
 		
-	this(string message, string sender){ 
-		this(Clock.currTime, message, sender); 
+	this(int ID, string message, string sender){ 
+		this(Clock.currTime, ID, message, sender); 
 	}
 
+	//Time at which the response was created
 	SysTime time;
+	
+	//The unique ID for this message
 	int ID;
+
+	//The content
 	string message;
+
+	//Handle of the user who sent this response
 	string sender;
 }
  
 interface ResponseAPI {
-	//Accessible by: GET localhost:8080/queue -- Returns a JSON object
-	ResponseQueue getQueue();
+	//GET: return a list of known servers
+	string[] getList();
 
-	//Accessible by: PUT localhost:8080/add {"message":"..."} -- Must be JSON formatted.
-	@property void add(string message, string sender);
+	//GET: return the queue for a specific server
+	ResponseQueue getQueue(string server);
 
-	//For testing purposes (kill the server)
-	@property void die();
+	//PUT: add an item to a servers queue
+	void putAdd(string server, string message, string sender);
 
-	//Remove an element
-	@property void remove(int ID);
+	//PUT: make a new server
+	void putMake(string name);
 
-	string getFoo();
+	//PUT: kill the main server
+	void putDie();
+
+	//PUT: remove an item from a servers queue
+	void putRemove(string server, int ID);
 }
  
-class ResponseImplementation : ResponseAPI {
-	this(string server_name){
-		responseQueue = ResponseQueue(server_name);
+class API : ResponseAPI {
+	this(string name){
+		servers[name] = ResponseQueue(name);
 		setTimer(15.seconds, &removeItems, true);
 	}	
 
-	protected ResponseQueue responseQueue;
- 	
-	ResponseQueue getQueue(){
-		logInfo("Queue requested at %s", Clock.currTime.toString);
-		return responseQueue;
+ 	protected ResponseQueue[string] servers;
+
+	enum timeout = 1.minutes; //30.minutes;
+	private void removeItems(){
+		size_t i = 0;
+		auto now = Clock.currTime;
+		logInfo("Starting cleanup at %s", now.toString);	
+
+		auto keys = servers.keys;
+
+		//Enumerate each server to check for cleanup		
+		foreach(key; keys){
+			auto server = servers[key];
+			auto IDs = server.queue.keys;
+			
+			foreach(ID; IDs){
+				if(now - server.queue[ID].time > timeout){
+					logInfo("\tRemoval - ID: %s", ID);
+					server.queue.remove(ID);
+				}
+			}
+		}
 	}
 
-	string getFoo(){
-		import vibe.data.json;
-		return responseQueue.serializeToPrettyJson;
+	override:
+
+	string[] getList(){
+		logInfo("Get list request\n\tSuccess");
+		return servers.keys;
+	}
+
+	ResponseQueue getQueue(string server){
+		logInfo("Get queue request: Server: %s, Time: %s", server, Clock.currTime.toString);
+		if(server !in servers){ 
+			logInfo("\tSpecified server does not exist");			
+			throw new HTTPStatusException(400, "Specified server does not exist"); 
+		}
+		logInfo("\tSuccess");
+		return servers[server];
 	}
  
-	@property void add(string message, string sender){
-		logInfo("%s added '%s' at %s", sender, message, Clock.currTime.toString);
-		responseQueue.queue ~= Response(message, sender);
+	void putAdd(string server, string message, string sender){
+		logInfo("Add request - Server: %s, Message: %s, Sender: %s, Time: %s", server, message, sender, Clock.currTime.toString);		
+		if(server !in servers){
+			logInfo("\tSpecified server does not exist");	
+			throw new HTTPStatusException(400, "Specified server does not exist"); 
+		}		
+		auto ID = servers[server].next_id++;		
+		servers[server].queue[ID] = Response(ID, message, sender);
+		logInfo("\tSuccessfully added");
 	}
 
-	@property void die(){
+	void putMake(string server){
+		logInfo("Make request - Server: %s, Time: %s", server, Clock.currTime.toString);
+		if(server in servers){ 
+			logInfo("\tSpecified server already exists");			
+			throw new HTTPStatusException(400, "Specified server already exists"); 
+		}
+		servers[server] = ResponseQueue(server);
+		logInfo("\tSuccessfully created");
+	}
+
+	void putDie(){
 		import std.c.stdlib;
 		logInfo("Server going down at %s", Clock.currTime.toString);
 		exit(0);
 	}
 
-	@property void remove(int ID){
-		size_t j = -1;
-		logInfo("Removal requested at %s", Clock.currTime.toString);
-		foreach(i, r; responseQueue.queue){
-			if(r.ID == ID){ j = i; break; }
+	void putRemove(string server, int ID){
+		logInfo("Removal request - Server: %s, ID: %s, Time: %s", server, ID, Clock.currTime.toString);		
+		if(server !in servers) { 
+			logInfo("\tSpecified server does not exist");			
+			throw new HTTPStatusException(400, "Specified server does not exist"); 
 		}
-
-		if(j!=-1){
-			responseQueue.queue = responseQueue.queue[0..j] ~ responseQueue.queue[j+1..$];
-			logInfo("\tRemoval made at %s", j);
+		else if(ID !in servers[server].queue){
+			logInfo("\tSpecified ID does not exist"); 
+			throw new HTTPStatusException(400, "Specified ID does not exist"); 
 		}
-		else{
-			logInfo("\tNo removal made, couldn't find index");
-		}
-	}
-
-	enum timeout = 30.minutes;
-	private void removeItems(){
-		import core.time;
-		
-		size_t i = 0;
-		auto now = Clock.currTime;
-		logInfo("Starting cleanup at %s", now.toString);
-		while(i < responseQueue.queue.length){
-			if(now - responseQueue.queue[i].time > timeout){
-				responseQueue.queue = responseQueue.queue[0..i] ~ responseQueue.queue[i+1..$];
-				logInfo("\tMade removal at index %s", i);
-			}
-			else i++;
-		}
+		servers[server].queue.remove(ID);
+		logInfo("\tSuccessfully removed");
 	}
 }
  
 shared static this()
 {
 	auto router = new URLRouter;
-	router.registerRestInterface(new ResponseImplementation("Test Server"));
+	router.registerRestInterface(new API("Test"));
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
 	listenHTTP(settings, router);
