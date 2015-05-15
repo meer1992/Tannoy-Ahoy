8e341696fa681b9c96fa5e617406349815a16e93
Anthony Meehan <antomeiq@gmail.com>
2015-05-15 20:55:49 +1200

    Merge in proper networking, adjusted JSON parser
    also included minor adjustments to the application design.

--- TannoyAhoy/app/src/main/java/com/uow/tannoyahoy/JsonParser.java
+++ TannoyAhoy/app/src/main/java/com/uow/tannoyahoy/JsonParser.java
@@ -5,38 +5,62 @@ import android.util.Log;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 
 /**
  * Created by Isa on 9/05/2015.
+ * Adjusted on 14/5/15.
  */
 public class JsonParser {
 
-    private static final String TAG_SENDER = "sender";
+    private static final String TAG_NAME = "name";
     private static final String TAG_QUEUE = "queue";
     private static final String TAG_TIME = "time";
     private static final String TAG_MESSAGE = "message";
     private String sender_ = "";
     private List<Data> lstData_ = new LinkedList<Data>();
     public JsonParser(String strJsonText) {
+
         try {
 
             // Creates a new JSONObject with name/value mappings from the JSON string.
             JSONObject jsonObj = new JSONObject(strJsonText);
 
+
             // Grabs the sender and saves it to the sender string variable
-            sender_ = new String(jsonObj.optString(TAG_SENDER).toString());
+            sender_ = jsonObj.optString(TAG_NAME);
+
+
 
             // Returns the value mapped by name if it exists and is a JSONArray. Returns null otherwise.
-            JSONArray jsonMainNode = jsonObj.optJSONArray(TAG_QUEUE);
+            //JSONArray jsonMainNode = jsonObj.optJSONArray(TAG_QUEUE);
+            JSONObject jsonMainNode = jsonObj.optJSONObject(TAG_QUEUE);
             //creates array of data
-             lstData_ = new LinkedList<Data>();
+            lstData_ = new LinkedList<Data>();
 
             // Process each JSON Node
-            int lengthJsonArr = jsonMainNode.length();
+            //int lengthJsonArr = jsonMainNode.length();
+            Iterator<String> theKeys = jsonMainNode.keys();
 
+            //New JSON parser code
+            while(theKeys.hasNext()){
+                String theCurrentKey = theKeys.next();
+                JSONObject jsonChildNode = jsonMainNode.getJSONObject(theCurrentKey);
+
+                // Fetch node values
+                String date = jsonChildNode.optString(TAG_TIME);
+                String message = jsonChildNode.getString(TAG_MESSAGE);
+                // Adding to list
+                lstData_.add(new Data(date,message));
+            }
+
+            //Old JSON parser code
+
+            /*
             for (int i = 0; i < lengthJsonArr; i++) {
                 // Get Object for each JSON node.
                 JSONObject jsonChildNode = jsonMainNode.getJSONObject(i);
@@ -48,56 +72,72 @@ public class JsonParser {
                 lstData_.add(new Data(date,message));
 
             }
+            */
 
             //writes list to debug if needed
             /*
             for (int i = 0; i < lstData_.size(); i++) {
                 Log.d("Json", lstData_.get(i).Message());
+
             }*/
 
-        } catch (JSONException e) {
-            e.printStackTrace();
+        } catch (JSONException jse) {
+
+            jse.printStackTrace();
+
         }
-        catch(Exception e) {
-            e.printStackTrace();
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
         }
     }
-
     public JsonParser(JSONObject JsonText) {
         try {
             // Creates a new JSONObject with name/value mappings from the JSON string.
-            sender_ = new String (JsonText.optString(TAG_SENDER).toString());
 
-            //Returns the value mapped by name if it exists and is a JSONArray. Returns null otherwise.
-            JSONArray jsonMainNode = JsonText.optJSONArray(TAG_QUEUE);
+
+            // Grabs the sender and saves it to the sender string variable
+            sender_ = JsonText.optString(TAG_NAME);
+
+
+
+            // Returns the value mapped by name if it exists and is a JSONArray. Returns null otherwise.
+
+            //JSONArray jsonMainNode = jsonObj.optJSONArray(TAG_QUEUE);
+            JSONObject jsonMainNode = JsonText.optJSONObject(TAG_QUEUE);
+            //creates array of data
             lstData_ = new LinkedList<Data>();
 
             // Process each JSON Node
-            int lengthJsonArr = jsonMainNode.length();
+            //int lengthJsonArr = jsonMainNode.length();
+            Iterator<String> theKeys = jsonMainNode.keys();
 
-            for (int i = 0; i < lengthJsonArr; i++) {
-                // Get Object for each JSON node.
-                JSONObject jsonChildNode = jsonMainNode.getJSONObject(i);
+            //New JSON parser code
+            while(theKeys.hasNext()){
+                String theCurrentKey = theKeys.next();
+                JSONObject jsonChildNode = jsonMainNode.getJSONObject(theCurrentKey);
 
                 // Fetch node values
                 String date = jsonChildNode.optString(TAG_TIME);
                 String message = jsonChildNode.getString(TAG_MESSAGE);
-                //Adding to list
+                // Adding to list
                 lstData_.add(new Data(date,message));
             }
-
             //writes list to debug if needed
             /*
             for (int i = 0; i < lstData_.size(); i++) {
                 Log.d("Json", lstData_.get(i).Message());
 
             }*/
+        } catch (JSONException jse) {
 
-        } catch (JSONException e) {
-            e.printStackTrace();
+            jse.printStackTrace();
+
         }
-        catch(Exception e) {
-            e.printStackTrace();
+        catch(Exception ex)
+        {
+
+            ex.printStackTrace();
         }
     }
     public List<Data> getList()
--- TannoyAhoy/app/src/main/java/com/uow/tannoyahoy/MainActivity.java
+++ TannoyAhoy/app/src/main/java/com/uow/tannoyahoy/MainActivity.java
@@ -6,24 +6,39 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 
 
 public class MainActivity extends ActionBarActivity {
     private TannoySpeech theTannoySpeech;
     private LocationBroadcastReceiver locationBroadcastReceiver;
-
+    public final static String LOG_TAG = "TannoyMainActivity";
     private final String strJsonParserExample =
             "{\"sender\":\"Test Server\",\"queue\":[{\"time\":\"2015-04-07T20:14:05.335358\",\"message\":\"This is a test message\"}" +
                                                   ",{\"time\":\"2015-04-07T20:14:56.4748071\",\"message\":\"This is some more stuff being added\"}" +
                                                   ",{\"time\":\"2015-04-07T20:15:26.4748071\",\"message\":\"Singapore Airlines SG03 gate changed from gate 3 to gate 4\"}]}";
-    private JsonParser theMainJsonParser;
+    private final static String strNewJsonExample =
+            "{\"name\":\"Test\",\"queue\":{\"128\":{\"time\":\"2015-05-14T16:11:15.0770064\",\"ID\":128,\"message\":\"first message in here\",\"sender\":\"hat\"},\"129\":{\"time\":\"2015-05-14T16:11:15.2080644\",\"ID\":129,\"message\":\"second message in here\",\"sender\":\"hat\"},\"130\":{\"time\":\"2015-05-14T16:11:15.3591318\",\"ID\":130,\"message\":\"third message in here\",\"sender\":\"hat\"},\"131\":{\"time\":\"2015-05-14T16:11:15.5392119\",\"ID\":131,\"message\":\"fourth message in here\",\"sender\":\"hat\"},\"132\":{\"time\":\"2015-05-14T16:11:15.702285\",\"ID\":132,\"message\":\"fifth message in here\",\"sender\":\"hat\"},\"133\":{\"time\":\"2015-05-14T16:11:15.913377\",\"ID\":133,\"message\":\"sixth message in here\",\"sender\":\"hat\"},\"134\":{\"time\":\"2015-05-14T16:11:16.0384326\",\"ID\":134,\"message\":\"seventh and final message in here\",\"sender\":\"hat\"}},\"next_id\":135}";
 
+    private JsonParser theMainJsonParser;
     private static final int SPEECH_REQUEST_CODE = 0;
 
     @Override
@@ -32,7 +47,8 @@ public class MainActivity extends ActionBarActivity {
         //define the activity layout.
         setContentView(R.layout.activity_main);
         //example of json call (can use a straight json object or a string formated to Json standard
-        theMainJsonParser = new JsonParser(strJsonParserExample);
+        //theMainJsonParser = new JsonParser(strNewJsonExample);
+        //Log.d(LOG_TAG, strJsonParserExample);
 
         //setup the location-backend
         locationBroadcastReceiver = new LocationBroadcastReceiver();
@@ -70,7 +86,7 @@ public class MainActivity extends ActionBarActivity {
      * @param menuItem
      **/
     public void settingsClicked (MenuItem menuItem) {
-        startActivity(new Intent(this,SettingsActivity.class));
+        startActivity(new Intent(this, SettingsActivity.class));
     }
     /**
      * This method is invoked when user clicks about menu option
@@ -88,12 +104,64 @@ public class MainActivity extends ActionBarActivity {
         startActivity(new Intent(this,LoginActivity.class));
     }
 
-    /**Updates the main list of messages when user clicks "update"*/
+    /**Gets the message queue when user clicks "update", then calls directUpdateListViewMain*/
     public void updateListViewMain(View theView) {
+        // TODO: Move this action into a singleton instance, and periodically update
+        RequestQueue theQueue = Volley.newRequestQueue(this);
+        //NOTE: 10.0.2.2 is for emulators only.
+        String theURL = "http://10.0.2.2:8080/queue?server=Test";
+
+        JsonObjectRequest theJsonRequest = new JsonObjectRequest
+                (Request.Method.GET, theURL, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(LOG_TAG, "HTTP Response is: " + response);
+
+                        theMainJsonParser = new JsonParser(response);
+                        directUpdateListViewMain();
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(LOG_TAG, "HTTP Request didn't work!" + error);
+                    }
+                });
+        //Old code handling a string instead. To be removed later on.
+        /*
+        StringRequest theStringRequest = new StringRequest
+                (Request.Method.GET, theURL,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Log the first 500 characters of the response string.
+                                Log.d(LOG_TAG, "HTTP Response is: " + response);
+
+                                theMainJsonParser = new JsonParser(response);
+                                directUpdateListViewMain();
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d(LOG_TAG, "HTTP Request didn't work!" + error);
+                            }
+                        });
+        */
+        theQueue.add(theJsonRequest);
+    }
+    /**Directly updates the main list of messages when user clicks "update"*/
+    public void directUpdateListViewMain(){
         //adapter which essentially calls toString() on a list of objects, then passes it into a listView
-        ArrayAdapter<Data> theAdapter = new ArrayAdapter<Data>(this,R.layout.list_item_main,theMainJsonParser.getList());
+        ArrayAdapter<Data> theAdapter;
+        theAdapter = new ArrayAdapter<Data>(this,R.layout.list_item_main,theMainJsonParser.getList());
         ListView theListview = (ListView) findViewById(R.id.listViewMain);
         theListview.setAdapter(theAdapter);
+    }
+    /**Clears the main list of messages when user clicks "clear"*/
+    public void clearListViewMain(View theView) {
+        ListView theListview = (ListView) findViewById(R.id.listViewMain);
+        theListview.setAdapter(null);
     }
 
     @Override
@@ -101,6 +169,7 @@ public class MainActivity extends ActionBarActivity {
         super.onDestroy();
         stopService(new Intent(App.context, BackgroundLocationService.class));
     }
+
     /**An example of using TannoySpeech. Buttons can call this with XML android:onClick="sendMessage"*/
 
     /*public void sendMessage(View theView) {
