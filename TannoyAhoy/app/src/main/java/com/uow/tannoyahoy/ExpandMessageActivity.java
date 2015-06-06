2acdfe84358f545a79a85c5fd548ec0765575836
Anthony Meehan <antomeiq@gmail.com>
2015-06-06 18:04:08 +1200

    Added removal of messages - for admin at least...
    ...also tweaked some UI

--- TannoyAhoy/app/src/main/java/com/uow/tannoyahoy/ExpandMessageActivity.java
+++ TannoyAhoy/app/src/main/java/com/uow/tannoyahoy/ExpandMessageActivity.java
@@ -3,15 +3,31 @@ package com.uow.tannoyahoy;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class ExpandMessageActivity extends ActionBarActivity {
 
     private TannoySpeech theTannoySpeech;
+    private int theMessageID;
+    private String currentSelectedZone = "";
+
+    private static final String REMOVE_TAG = "TannRemoveClick";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +37,15 @@ public class ExpandMessageActivity extends ActionBarActivity {
         // Get the message from the intent
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXPAND_MESSAGE);
+        theMessageID = intent.getIntExtra(MainActivity.EXPAND_ID,-1);
+
+        //Get the server
+        currentSelectedZone = intent.getStringExtra(MainActivity.EXPAND_SERVER);
 
         // Fill the text view
         TextView theTextView = (TextView) findViewById(R.id.textToExpand);
         theTextView.setText(message);
+        Log.d("GetID", "Expanded ID is:" + theMessageID);
 
         //Initialise Text To Speech
         theTannoySpeech = new TannoySpeech(this);
@@ -66,5 +87,50 @@ public class ExpandMessageActivity extends ActionBarActivity {
 
         theTannoySpeech.speak(theTextToRead);
 
+    }
+    /**remove messages from the server ***/
+    public void removeTheItem(View theView) {
+        // Get the RequestQueue
+        RequestQueue theQueue = TannoyRequestQueueSingleton.getInstance(getApplicationContext()).
+                getRequestQueue();
+
+        String theURL = Constants.REMOVEURL;
+
+        //try putting the JSON object together
+        JSONObject theJsonObject = new JSONObject();
+        try {
+
+            theJsonObject.put("server",currentSelectedZone);
+            theJsonObject.put("ID",theMessageID);
+            theJsonObject.put("username",Settings.getInstance().getUsername());
+            theJsonObject.put("password",Settings.getInstance().getPassword());
+
+        } catch (JSONException e) {
+            e.printStackTrace(); return;
+        }
+
+        Log.d(REMOVE_TAG, "Making removal: " + theJsonObject.toString());
+
+        //send the JSON REMOVE request
+        JsonObjectRequest theJsonRequest = new JsonObjectRequest
+                (Request.Method.PUT, theURL, theJsonObject, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(REMOVE_TAG, "HTTP Response is: " + response);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(REMOVE_TAG, "HTTP Request didn't work!" + error);
+                    }
+                });
+
+        theQueue.add(theJsonRequest);
+
+        //go back to main activity, while the remove request is being sent
+        Intent a = new Intent(theView.getContext(), MainActivity.class);
+        a.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(a);
+        Toast.makeText(getApplicationContext(), "Removal made", Toast.LENGTH_LONG).show();
     }
 }
